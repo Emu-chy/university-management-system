43de0d8ffd692f53e9ddeeebb11c4fc774772df0
Emon Chowdhury <emonchowdhury942858@gmail.com>
2023-06-17 18:27:50 +0600

    Modify Academic Semester Module

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,8 @@
 import cors from 'cors';
-import express, { Application } from 'express';
+import express, { Application, NextFunction, Request, Response } from 'express';
+import httpStatus from 'http-status';
 import globalErrorHandler from './app/middlewares/globalErrorHandler';
-import { AcademicSemesterRoutes } from './app/modules/academicSemester/academicSemester.routes';
-import { UserRoutes } from './app/modules/user/user.routes';
+import routes from './app/routes';
 
 // import ApiError from './errors/ApiError'
 const app: Application = express();
@@ -12,13 +12,29 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 //application routes
-app.use('/api/v1/users/', UserRoutes);
-app.use('/api/v1/academic-semesters', AcademicSemesterRoutes);
+// app.use('/api/v1/users/', UserRoutes);
+// app.use('/api/v1/academic-semesters', AcademicSemesterRoutes);
+app.use('/api/v1', routes);
 
 // app.get('/', async (req: Request, res: Response) => {
 //     Promise.reject(new Error("Unhandled Promise Rejection"))
 // })
 
 app.use(globalErrorHandler);
+
+//handle not found
+app.use((req: Request, res: Response, next: NextFunction) => {
+  res.status(httpStatus.NOT_FOUND).json({
+    success: false,
+    message: 'Not Found',
+    errorMessages: [
+      {
+        path: req.originalUrl,
+        message: 'API Not Found',
+      },
+    ],
+  });
+  next();
+});
 
 export default app;
--- src/app/modules/academicSemester/academicSemester.controller.ts
+++ src/app/modules/academicSemester/academicSemester.controller.ts
@@ -1,21 +1,27 @@
-import { RequestHandler } from 'express';
+import { NextFunction, Request, Response } from 'express';
+import httpStatus from 'http-status';
+import catchAsync from '../../../shared/catchAsync';
+import sendResponse from '../../../shared/sendResponse';
+import { IAcademicSemester } from './academicSemester.interface';
 import { AcademicSemesterService } from './academicSemester.service';
 
-const createSemester: RequestHandler = async (req, res, next) => {
-  try {
+const createSemester = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
     const { ...academicSemesterData } = req.body;
     const result = await AcademicSemesterService.createSemester(
       academicSemesterData
     );
-    res.status(200).json({
+
+    sendResponse<IAcademicSemester>(res, {
+      statusCode: httpStatus.OK,
       success: true,
-      message: 'Academic Semester is  created successfully',
+      message: 'Academic Semester is Created Successfully!',
       data: result,
     });
-  } catch (error) {
-    next(error);
+
+    next();
   }
-};
+);
 
 export const AcademicSemesterController = {
   createSemester,
